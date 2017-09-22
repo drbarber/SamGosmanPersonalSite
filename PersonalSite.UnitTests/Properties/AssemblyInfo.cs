8a4d6a28f7cc2d7b18b0350941d2e45c8ec99fdd
Sam Gosman <sam.gosman@ZIRMED.COM>
2017-09-22 13:31:11 -0400

    Add project files.

---
+++ PersonalSite.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PersonalSite.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PersonalSite.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19ca9d3f-cf29-43a1-8c5a-888ef58c5bbf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PersonalSite.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalSite.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HandleError]
+        public ViewResult Index()
+        { 
+            return View();
+        }
+        [HandleError]
+        public ViewResult Resume()
+        {
+            return View();
+        }
+        [HandleError]
+        public ViewResult FourOhFour()
+        {
+            return View();
+        }
+        [HandleError]
+        public ViewResult SocialMedia()
+        {
+            return View();
+        }
+        [HandleError]
+        public ViewResult Contact()
+        {
+            return View();
+        }
+        [HandleError]
+        public PartialViewResult Menu()
+        {
+            
+            return PartialView();
+        }
+
+    }
+}
