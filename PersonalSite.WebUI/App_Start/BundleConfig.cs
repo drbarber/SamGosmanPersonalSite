01f92f9919d31818f48c5e8bb0007a7ad1ff9d67
Sam Gosman <sgosman506@gmail.com>
2017-09-25 13:44:27 -0400

    Bundling

---
+++ PersonalSite.WebUI/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Optimization;
+
+namespace PersonalSite.WebUI.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css"));
+            bundles.Add(new ScriptBundle("~/Scripts").Include("~/Scripts/*.js"));
+        }
+    }
+}
