f333e783412bc5e6b18fa8cf0acb3680b97406f0
Sam Gosman <sgosman506@gmail.com>
2017-10-02 13:14:56 -0400

    *added new background image *centered text and images

--- PersonalSite.WebUI/Scripts/MoveImage.js
+++ PersonalSite.WebUI/Scripts/MoveImage.js
@@ -1,12 +1,15 @@
 ﻿$(document).ready(function () {
+    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
     var movementStrength = 25;
     var height = movementStrength / $(window).height();
     var width = movementStrength / $(window).width();
     $("#top-image").mousemove(function (e) {
-        var pageX = e.pageX - ($(window).width() / 2);
-        var pageY = e.pageY - ($(window).height() / 2);
-        var newvalueX = width * pageX * -1 - 50;
-        var newvalueY = height * pageY * -1 - 75;
-        $('#top-image').css("background-position", newvalueX + "px     " + newvalueY + "px");
+        if (!isMobile) {
+            var pageX = e.pageX - ($(window).width() / 2);
+            var pageY = e.pageY - ($(window).height() / 2);
+            var newvalueX = width * pageX * -1 - 50;
+            var newvalueY = height * pageY * -1 - 75;
+            $('#top-image').css("background-position", newvalueX + "px     " + newvalueY + "px");
+        }
     });
 });
